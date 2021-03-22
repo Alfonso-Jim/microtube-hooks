3084fff7ba4039932812136671a65fc882152a56
Alfonso Jim <alfonsojim1897@gmail.com>
2021-03-22 15:39:57 +0100

    refactored class based comp to functional comp

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,36 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./VideoItem.css";
 
-class SearchBar extends React.Component {
-  state = { term: "" };
+const Search = ({ onTermSubmit }) => {
+  const [term, setTerm] = useState("");
 
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-  };
-
-  onFormSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
 
-    this.props.onTermSubmit(this.state.term);
+    onTermSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar inverted ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <h1 className="ui header grey inverted ">MicroTube</h1>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-              placeholder="Search for some videos..."
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="search-bar inverted ui segment">
+      <form onSubmit={onSubmit} className="ui form">
+        <div className="field">
+          <h1 className="ui header grey inverted ">MicroTube</h1>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+            placeholder="Search for some videos..."
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
